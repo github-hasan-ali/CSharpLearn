d48a0a75129d3499a38049c840d9743dc63cb97b
h. ali <gumanest@hotmail.com>
2020-05-10 01:36:35 +0300

    Added comment

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -10,7 +10,8 @@ namespace CSharpLearn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+
             Console.ReadLine();
         }
     }
