40e0d715f8f7634fcbcb9fff7e193f6affb7750b
h. ali <gumanest@hotmail.com>
2020-05-10 04:11:35 +0300

    Added Methods Class

--- CSharpLearn/Program.cs
+++ CSharpLearn/Program.cs
@@ -11,6 +11,19 @@ namespace CSharpLearn
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World");
+            var number = 10;
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("Number is 10");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("Number is 20");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Number is no 10 or 20");
+            //        break;
+            //}
 
             Console.ReadLine();
         }
