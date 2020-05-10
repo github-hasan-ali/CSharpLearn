744346ad5ba7a9781aea2fca6dcd1cfb12b6378a
h. ali <gumanest@hotmail.com>
2020-05-10 04:43:29 +0300

    Methots continue

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,6 +10,21 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            var result = Add2(20);
+            Console.WriteLine("Toplam= {0}", result);
+            Console.ReadLine();
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+
+        static int Add2(int number1,int number2=42)
+        {
+            var result = number1 + number2;
+            return result;
+
         }
     }
 }
