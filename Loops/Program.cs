9862056307bcd87cd0b1f7217986099f5d57138f
h. ali <gumanest@hotmail.com>
2020-05-10 18:12:50 +0300

    Continue

---
+++ Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            string[] students =new string[3] {"hasan","ali","kart"};
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
